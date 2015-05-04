7fe8039b3a102dee1a06e05fe1c94f8f3119b011
Brent Christensen <brent@eventespresso.com>
2015-05-04 16:53:13 -0700

    allow phone number regex and example to be set in options and filtered; [touch:8065]

--- core/libraries/form_sections/inputs/EE_Phone_Input.input.php
+++ core/libraries/form_sections/inputs/EE_Phone_Input.input.php
@@ -19,8 +19,20 @@ class EE_Phone_Input extends EE_Text_Input{
 	/**
 	 * @param array $options
 	 */
-	function __construct($options = array()){
-		$this->_add_validation_strategy( new EE_Text_Validation_Strategy( __( 'Please enter a valid phone number. Eg 123-456-7890 or 1234567890', 'event_espresso' ), '~^([\d]{10})|([\d]{3}-[\d]{3}-[\d]{4})$~'));
+	function __construct( $options = array() ){
+		//set and filter regex
+		$phone_number_regex = isset( $options[ 'phone_number_regex' ] ) ? $options[ 'phone_number_regex' ] : '~^([\d]{10})|([\d]{3}-[\d]{3}-[\d]{4})$~';
+		$phone_number_regex = apply_filters( 'FHEE__EE_Phone_Input____construct__phone_number_regex', $phone_number_regex, $this );
+		//set and filter example
+		$phone_number_example = isset( $options[ 'phone_number_example' ] ) ? $options[ 'phone_number_example' ] : '123-456-7890 or 1234567890';
+		$phone_number_example = apply_filters( 'FHEE__EE_Phone_Input____construct__phone_number_example', $phone_number_example, $this );
+		// add strategy
+		$this->_add_validation_strategy(
+			new EE_Text_Validation_Strategy(
+				sprintf( __( 'Please enter a valid phone number. Example: %1$s', 'event_espresso' ), $phone_number_example ),
+				$phone_number_regex
+			)
+		);
 		parent::__construct( $options );
 	}
 }
